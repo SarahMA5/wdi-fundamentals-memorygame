9070687d3c34f39da9a9d8b5af91dcfaf8136c48
Sarah <alahmadisr@gmail.com>
2019-11-10 08:01:20 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,45 +1,51 @@
-
-var cards=[
+//Create an array of cards objects.
+var cards=[ 
 {rank:"queen",suit:"hearts",cardImage:"images/queen-of-hearts.png"},
 {rank:"queen",suit:"diamonds",cardImage:"images/queen-of-diamonds.png"},
 {rank:"king",suit:"hearts",cardImage:"images/king-of-hearts.png"},
 {rank:"king",suit:"diamonds",cardImage:"images/king-of-diamonds.png"} ]
-var cardsInPlay=[];
 
+//Create an array that holds a references to the chosen cards.
+var cardsInPlay=[]; 
 
+//-----------------------------------------------------------------------------
 
 function checkForMatch(){
-if(cardsInPlay[0]===cardsInPlay[1]){
+	if(cardsInPlay[0]===cardsInPlay[1]){
 		alert("You found a match!");
-		//console.log("You found a match!");
 	}
 	else{
 		alert("Sorry, try again.");
-		//console.log("Sorry, try again.");
 	}
-} //End of outer if statement.
+} //End of checkForMatch function.
 
+//-----------------------------------------------------------------------------
 
 function flipCard(){
-var cardId=this.getAttribute('data-id');
-this.setAttribute('src',cards[cardId].cardImage); //Update to show the card.
+	var cardId=this.getAttribute('data-id');
+this.setAttribute('src',cards[cardId].cardImage); //Update the image source to show the card.
+
+/*
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-
+*/
 
 cardsInPlay.push(cards[cardId].rank);
 
 if(cardsInPlay.length===2){
 	checkForMatch();
 	
-
+} 
 } //End of flipCard Function.
-}
 
+//-----------------------------------------------------------------------------
 
+//createBoard function initializes the game board by creating and adding img
+//elements representing the cards to the div element where the game board is located.
 function createBoard(){
 	for(var i=0;i<cards.length;i++){
+
 		var cardElement=document.createElement('img');
 		cardElement.setAttribute('src',"images/back.png");
 		cardElement.setAttribute('data-id',i);
@@ -48,6 +54,8 @@ function createBoard(){
 
 	}
 }
+
+//-----------------------------------------------------------------------------
 
 createBoard();
 
