afbbd82b75be65d27b4baea47a052f3e77d6ac00
Sarah <alahmadisr@gmail.com>
2019-11-10 08:04:21 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,10 @@
-//Create an array of cards objects.
 var cards=[ 
 {rank:"queen",suit:"hearts",cardImage:"images/queen-of-hearts.png"},
 {rank:"queen",suit:"diamonds",cardImage:"images/queen-of-diamonds.png"},
 {rank:"king",suit:"hearts",cardImage:"images/king-of-hearts.png"},
 {rank:"king",suit:"diamonds",cardImage:"images/king-of-diamonds.png"} ]
 
-//Create an array that holds a references to the chosen cards.
+
 var cardsInPlay=[]; 
 
 //-----------------------------------------------------------------------------
